9576f2b6e0ff992698ce62ae418e05eea9f98e2f
Jesse Sopel <jesse.sopel@gmail.com>
2020-11-24 18:57:50 -0500

    feat: Add waitlist API

--- custom_components/de_occupancy/const.py
+++ custom_components/de_occupancy/const.py
@@ -1,12 +1,14 @@
 from collections import namedtuple
 
-Gym = namedtuple('Gym', 'id code name')
+Gym = namedtuple('Gym', 'id code name waitlist')
 
 DOMAIN = "de_occupancy"
 CONF_GYMS = 'gyms'
 GYMS = {
-    'beauport': Gym('beauport', 'BPT', 'Beauport'),
-    'pierrebertrand': Gym('pierrebertrand', 'DPB', 'Pierre Bertrand'),
-    'stefoy': Gym('stefoy', 'DLE', 'Ste-Foy'),
+    'beauport': Gym('beauport', 'BPT', 'Beauport', 'beauport'),
+    'pierrebertrand': Gym('pierrebertrand', 'DPB', 'Pierre Bertrand', 'delire'),
+    'stefoy': Gym('stefoy', 'DLE', 'Ste-Foy', 'delireste-foy'),
 }
 OCCUPANCY_API_URL = "https://www.delirescalade.com/web/wp-json/api/occupancy?skipcache=1&code={code}"
+WAITLIST_API_URL = "https://api.waitwhile.com/v2/public/locations/{code}"
+WAITLIST_JOIN_URL = "https://v2.waitwhile.com/lists/delire/join"
--- custom_components/de_occupancy/sensor.py
+++ custom_components/de_occupancy/sensor.py
@@ -15,7 +15,7 @@ from homeassistant.helpers.typing import (
 )
 import voluptuous as vol
 
-from .const import CONF_GYMS, GYMS, OCCUPANCY_API_URL, Gym
+from .const import CONF_GYMS, GYMS, OCCUPANCY_API_URL, WAITLIST_API_URL, Gym
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -45,7 +45,7 @@ class DeOccupancySensor(Entity):
     def __init__(self, gym: str) -> None:
         super().__init__()
         self.gym: Gym = GYMS[gym]
-        self.attrs: Dict[str, Any] = {'gym': self.gym.name}
+        self.attrs: Dict[str, Any] = {}
         self._state = None
         self._available = True
 
@@ -85,22 +85,31 @@ class DeOccupancySensor(Entity):
     def device_state_attributes(self) -> Dict[str, Any]:
         return self.attrs
 
+    async def fetch_new_attrs(self):
+        try:
+            async with ClientSession() as session:
+                async with session.get(OCCUPANCY_API_URL.format(code=self.gym.code)) as response:
+                    data = await response.json()
+                
+                if data['percent'] >= 95:
+                    # Only check the waitlist if over 95%
+                    async with session.get(WAITLIST_API_URL.format(code=self.gym.waitlist)) as response:
+                        waitlist_data = await response.json()
+                    data['waiting'] = waitlist_data['numWaiting']  # number on waitlist
+                    data['wait_eta'] = waitlist_data['wait']  # seconds
+
+        except ClientError:
+            self._available = False
+            _LOGGER.exception("Error retrieving data from DE API.")
+            return {}
+
     async def async_update(self) -> None:
+        attrs = {'count': 0, 'percent': 0, 'waiting': 0, 'wait_eta': 0, 'gym': self.gym.name}
         if 8 <= datetime.now().hour <= 22:
-            try:
-                async with ClientSession() as session:
-                    async with session.get(OCCUPANCY_API_URL.format(code=self.gym.code)) as response:
-                        data = await response.json()
-            except ClientError:
-                self._available = False
-                _LOGGER.exception("Error retrieving data from DE API.")
-                return None
-        else:
-            # Closed always empty
-            data = {'count': 0, 'percent': 0, 'full_percent': 0}
+            # Only fetch when open
+            attrs.update(await self.fetch_new_attrs())
         
         # set values
-        self.attrs['count'] = data['count']
-        self.attrs['full_percent'] = data['percent']
-        self._state = 100 if (percent := round(data['percent'])) >= 100 else percent
+        self.attrs = attrs
+        self._state = 100 if (percent := round(attrs['percent'])) >= 100 else percent
         self._available = True
